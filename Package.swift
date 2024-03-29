ec9e4749686f24fe42d753d09b9bc3a928983d0f
Jomy10 <jonas.vbs4@gmail.com>
2024-03-29 14:49:29 +0100

    remove submodule

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "photobooth",
     dependencies: [
         .package(url: "https://github.com/jomy10/swift-graphics", branch: "main"),
-        .package(url: "https://github.com/fwcd/swift-cairo", from: "1.3.4"),
+        .package(url: "https://github.com/jomy10/swift-cairo", branch: "main"),
         .package(url: "https://github.com/fwcd/swift-utils.git", from: "3.0.0"),
 
         .package(url: "https://github.com/troughton/Cstb", branch: "main"),
@@ -55,11 +55,11 @@ let package = Package(
         ),
 
         // Graphics //
-        // .target(
-        //     name: "CairoJPG",
-        //     path: "Sources/Graphics/CairoJPG",
-        //     publicHeadersPath: "Sources/Graphics/CairoJPG/src"
-        // ),
+        .target(
+            name: "CairoJPG",
+            path: "Sources/Graphics/CairoJPG",
+            publicHeadersPath: "Sources/Graphics/CairoJPG/src"
+        ),
 
         // Multithreading //
         .target(
