50b17a7ca5a554afac86b5aa8c1c42992cac48f6
Jomy10 <jonas.vbs4@gmail.com>
2024-03-29 13:52:33 +0100

    build command

--- Package.swift
+++ Package.swift
@@ -1,26 +1,70 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 
 import PackageDescription
 
 let package = Package(
     name: "photobooth",
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/jomy10/swift-graphics", branch: "main"),
+        .package(url: "https://github.com/fwcd/swift-cairo", from: "1.3.4"),
+        .package(url: "https://github.com/fwcd/swift-utils.git", from: "3.0.0"),
+
+        .package(url: "https://github.com/troughton/Cstb", branch: "main"),
+    ],
     targets: [
         .executableTarget(
-            name: "photobooth-new",
+            name: "Photobooth",
             dependencies: [
-                "framebuffer"
+                "Framebuffer",
+                "drm",
+
+                "Input",
+
+                .product(name: "CairoGraphics", package: "swift-graphics"),
+                .product(name: "Cairo", package: "swift-cairo"),
+                .product(name: "Utils", package: "swift-utils"),
+
+                .product(name: "stb_image", package: "Cstb"),
+                .product(name: "stb_image_resize", package: "Cstb"),
             ]
         ),
-        .target(
-            name: "drm",
-            path: "Sources/framebuffer/drm"
-        ),
+
+        // Framebuffer //
         .target(
             name: "Framebuffer",
             dependencies: ["drm"],
-            path: "Sources/framebuffer/wrapper"
-        )
+            path: "Sources/Framebuffer/wrapper"
+        ),
+        .target(
+            name: "drm",
+            path: "Sources/Framebuffer/drm"
+        ),
+
+        // Input //
+        .target(
+            name: "Input",
+            dependencies: [
+                "LibInput",
+                "Thread"
+            ],
+            path: "Sources/Input/wrapper"
+        ),
+        .target(
+            name: "LibInput",
+            path: "Sources/Input/LibInput"
+        ),
+
+        // Graphics //
+        // .target(
+        //     name: "CairoJPG",
+        //     path: "Sources/Graphics/CairoJPG",
+        //     publicHeadersPath: "Sources/Graphics/CairoJPG/src"
+        // ),
+
+        // Multithreading //
+        .target(
+            name: "Thread"
+        ),
     ]
 )
 
